56ff526f545530e900c8500497fe8265b1cf73a0
Gi SC DHS <gisc@dhs.sg>
2012-07-18 21:44:29 +0800

    updated comments

--- monthly_statement.py
+++ monthly_statement.py
@@ -5,6 +5,7 @@ from classes import *
 # main
 if __name__ == "__main__":
     try:
+        # open files for input
         account_file = open("ACCOUNTS.DAT", 'r')
         transaction_file = open("TRANSACTIONS.DAT", 'r') 
 
@@ -16,6 +17,7 @@ if __name__ == "__main__":
         
         # loop through all accounts
         for account_line in account_lines:
+            # get account details
             account_no = account_line[:6]
             customer_name = account_line[6:35]
             balance = float(account_line[35:])
@@ -27,17 +29,21 @@ if __name__ == "__main__":
                 account = CurrentAccount(account_no, customer_name, balance)
             #account.display()
 
+            # loop through all transactions
             for transaction_line in transaction_lines:
+                # get transaction details
                 transaction_date = transaction_line[:8]
                 transaction_account = transaction_line[8:14]
                 transaction_type = transaction_line[14:15]
                 transaction_amount = float(transaction_line[15:])
+                # if relevant account
                 if transaction_account == account_no:
-                    if transaction_type == 'D':
+                    if transaction_type == 'D': # deposit
                         account.deposit(transaction_amount)
                     else: # withdraw
                         account.withdraw(transaction_amount)
-                        
+
+            # output monthly statement
             account.display_monthly_statement()
             print()
 
